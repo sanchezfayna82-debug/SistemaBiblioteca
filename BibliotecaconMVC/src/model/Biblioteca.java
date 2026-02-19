b5c05472d8ad036e16c3cc98decdd9a06e02b90b
marouanbougataya38-cmyk <marouanbougataya38@gmail.com>
2026-02-19 17:45:58 +0000

    Add files via upload

---
+++ BibliotecaconMVC/src/model/Biblioteca.java
@@ -0,0 +1,21 @@
+package model;
+
+import java.util.ArrayList; 
+import model.Libro;
+
+public class Biblioteca {
+
+    private ArrayList<Libro> libros = new ArrayList<>();  
+
+    public void agregarLibro(Libro libro) {
+        libros.add(libro);  
+    }
+
+    public ArrayList<String> listarLibros() {
+        ArrayList<String> lista = new ArrayList<>();
+        for (Libro libro : libros) {  
+            lista.add(libro.toString());  
+        }
+        return lista;
+    }
+}
